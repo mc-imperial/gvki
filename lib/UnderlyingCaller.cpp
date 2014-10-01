0b84b61d5475e3df599d60676f3ba771f44cb567
Dan Liew <daniel.liew@imperial.ac.uk>
2014-10-01 12:13:11 +0100

    Revert "Under OSX add an underscore prefix for the function name being passed"
    It has been reported that this (not surprisingly) doesn't fix the issue.
    
    This reverts commit aa2c21b9a7a081876a80dfca1c9711e6e1856420.

--- lib/UnderlyingCaller.cpp
+++ lib/UnderlyingCaller.cpp
@@ -9,33 +9,20 @@
 using namespace gvki;
 
 #ifdef MACRO_LIB
-#define SET_FCN_PTR(functionName) functionName ## U = &(::functionName);
+#define SET_FCN_PTR(F) F ## U = &(::F);
 #else
 #include <unistd.h>
-#define _SET_FCN_PTR(functionName, symbol) dlerror(); /* clear any old errors */ \
-                       functionName ## U = reinterpret_cast<functionName ## Ty>(::dlsym(RTLD_NEXT, #symbol)); \
+#define SET_FCN_PTR(F) dlerror(); /* clear any old errors */ \
+                       F ## U = reinterpret_cast<F ## Ty>(::dlsym(RTLD_NEXT, #F)); \
                        errorMsg = dlerror(); \
                        if ( errorMsg != NULL) { \
-                         std::cerr << "Failed to dlsym(\"" #symbol  "\"): " << errorMsg << std::endl; \
+                         std::cerr << "Failed to dlsym(\"" #F  "\"): " << errorMsg << std::endl; \
                          _exit(255); \
                        } \
-                       if ( functionName ## U == NULL) { \
-                         std::cerr << "Function pointer for \"" #functionName "\" cannot be NULL" << std::endl; \
+                       if ( F ## U == NULL) { \
+                         std::cerr << "Function pointer for \"" #F "\" cannot be NULL" << std::endl; \
                          _exit(255); \
                        }
-
-#ifdef __APPLE__
-// Under OSX the OpenCL symbols are prefixed with an underscore [1] claims that
-// dlsym() is supposed to handle that for us but testing by others seemed to
-// suggest this wasn't the case.
-//
-// [1] https://developer.apple.com/library/mac/documentation/Darwin/Reference/ManPages/man3/dlsym.3.html
-#define SET_FCN_PTR(functionName) _SET_FCN_PTR(functionName, _ ## functionName)
-#else
-#define SET_FCN_PTR(functionName) _SET_FCN_PTR(functionName, functionName)
-#endif
-
-
 #endif
 
 UnderlyingCaller::UnderlyingCaller()
