aa2c21b9a7a081876a80dfca1c9711e6e1856420
Dan Liew <daniel.liew@imperial.ac.uk>
2014-10-01 11:54:15 +0100

    Under OSX add an underscore prefix for the function name being passed to dlsym(). [1] suggests this shouldn't be necessary but testing seems to have shown that dlsym() wasn't working under OSX. This is an attempt to see if this fixes the issue
    This has not been tested at all!
    
    [1] https://developer.apple.com/library/mac/documentation/Darwin/Reference/ManPages/man3/dlsym.3.html

--- lib/UnderlyingCaller.cpp
+++ lib/UnderlyingCaller.cpp
@@ -9,20 +9,33 @@
 using namespace gvki;
 
 #ifdef MACRO_LIB
-#define SET_FCN_PTR(F) F ## U = &(::F);
+#define SET_FCN_PTR(functionName) functionName ## U = &(::functionName);
 #else
 #include <unistd.h>
-#define SET_FCN_PTR(F) dlerror(); /* clear any old errors */ \
-                       F ## U = reinterpret_cast<F ## Ty>(::dlsym(RTLD_NEXT, #F)); \
+#define _SET_FCN_PTR(functionName, symbol) dlerror(); /* clear any old errors */ \
+                       functionName ## U = reinterpret_cast<functionName ## Ty>(::dlsym(RTLD_NEXT, #symbol)); \
                        errorMsg = dlerror(); \
                        if ( errorMsg != NULL) { \
-                         std::cerr << "Failed to dlsym(\"" #F  "\"): " << errorMsg << std::endl; \
+                         std::cerr << "Failed to dlsym(\"" #symbol  "\"): " << errorMsg << std::endl; \
                          _exit(255); \
                        } \
-                       if ( F ## U == NULL) { \
-                         std::cerr << "Function pointer for \"" #F "\" cannot be NULL" << std::endl; \
+                       if ( functionName ## U == NULL) { \
+                         std::cerr << "Function pointer for \"" #functionName "\" cannot be NULL" << std::endl; \
                          _exit(255); \
                        }
+
+#ifdef __APPLE__
+// Under OSX the OpenCL symbols are prefixed with an underscore [1] claims that
+// dlsym() is supposed to handle that for us but testing by others seemed to
+// suggest this wasn't the case.
+//
+// [1] https://developer.apple.com/library/mac/documentation/Darwin/Reference/ManPages/man3/dlsym.3.html
+#define SET_FCN_PTR(functionName) _SET_FCN_PTR(functionName, _ ## functionName)
+#else
+#define SET_FCN_PTR(functionName) _SET_FCN_PTR(functionName, functionName)
+#endif
+
+
 #endif
 
 UnderlyingCaller::UnderlyingCaller()
