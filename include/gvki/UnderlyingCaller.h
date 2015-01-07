9ba90bb725b672905d647d9c15ab234f9fb206e0
Dan Liew <daniel.liew@imperial.ac.uk>
2015-01-07 14:01:56 +0000

    Fix assignment to UnderlyingCaller function pointers under Windows. It seems that when we take the address of an OpenCL API function the type is (__stdcall*) rather than (*).
    We hackishly reuse OpenCL's CL_CALLBACK macro to define this when
    necessary.

--- include/gvki/UnderlyingCaller.h
+++ include/gvki/UnderlyingCaller.h
@@ -6,114 +6,116 @@
 namespace gvki
 {
 
+// FIXME: Should we define our own macro rather than abusing
+// CL_CALLBACK?
 class UnderlyingCaller
 {
     public:
-        typedef cl_mem (*clCreateBufferTy)(cl_context,
-                                           cl_mem_flags,
-                                           size_t,
-                                           void*,
-                                           cl_int*);
+        typedef cl_mem (CL_CALLBACK *clCreateBufferTy)(cl_context,
+                                                       cl_mem_flags,
+                                                       size_t,
+                                                       void*,
+                                                       cl_int*);
         clCreateBufferTy clCreateBufferU;
 
-        typedef cl_mem (*clCreateSubBufferTy)(cl_mem,
-                                              cl_mem_flags,
-                                              cl_buffer_create_type,
-                                              const void*,
-                                              cl_int*
-                                             );
+        typedef cl_mem (CL_CALLBACK *clCreateSubBufferTy)(cl_mem,
+                                                          cl_mem_flags,
+                                                          cl_buffer_create_type,
+                                                          const void*,
+                                                          cl_int*
+                                                         );
         clCreateSubBufferTy clCreateSubBufferU;
 
-        typedef cl_mem (*clCreateImage2DTy)(cl_context,
-                                            cl_mem_flags,
-                                            const cl_image_format*,
-                                            size_t,
-                                            size_t,
-                                            size_t,
-                                            void*,
-                                            cl_int*
-                                           );
-        clCreateImage2DTy clCreateImage2DU;
+        typedef cl_mem (CL_CALLBACK *clCreateImage2DTy)(cl_context,
+                                                        cl_mem_flags,
+                                                        const cl_image_format*,
+                                                        size_t,
+                                                        size_t,
+                                                        size_t,
+                                                        void*,
+                                                        cl_int*
+                                                       );
+            clCreateImage2DTy clCreateImage2DU;
 
-        typedef cl_mem (*clCreateImage3DTy)(cl_context,
-                                            cl_mem_flags,
-                                            const cl_image_format*,
-                                            size_t,
-                                            size_t,
-                                            size_t,
-                                            size_t,
-                                            size_t,
-                                            void*,
-                                            cl_int*
-                                           );
+        typedef cl_mem (CL_CALLBACK *clCreateImage3DTy)(cl_context,
+                                                        cl_mem_flags,
+                                                        const cl_image_format*,
+                                                        size_t,
+                                                        size_t,
+                                                        size_t,
+                                                        size_t,
+                                                        size_t,
+                                                        void*,
+                                                        cl_int*
+                                                       );
         clCreateImage3DTy clCreateImage3DU;
 
 #ifdef CL_VERSION_1_2
-       typedef cl_mem (*clCreateImageTy)(cl_context context,
-                                         cl_mem_flags flags,
-                                         const cl_image_format* image_format,
-                                         const cl_image_desc* image_desc,
-                                         void* host_ptr,
-                                         cl_int*  errcode_ret
-                                        );
+       typedef cl_mem (CL_CALLBACK *clCreateImageTy)(cl_context context,
+                                                     cl_mem_flags flags,
+                                                     const cl_image_format* image_format,
+                                                     const cl_image_desc* image_desc,
+                                                     void* host_ptr,
+                                                     cl_int*  errcode_ret
+                                                    );
        clCreateImageTy clCreateImageU;
 #endif
 
-        typedef cl_sampler (*clCreateSamplerTy)(cl_context,
-                                                cl_bool,
-                                                cl_addressing_mode,
-                                                cl_filter_mode,
-                                                cl_int*
-                                               );
+        typedef cl_sampler (CL_CALLBACK *clCreateSamplerTy)(cl_context,
+                                                            cl_bool,
+                                                            cl_addressing_mode,
+                                                            cl_filter_mode,
+                                                            cl_int*
+                                                           );
         clCreateSamplerTy clCreateSamplerU;
 
-        typedef cl_program (*clCreateProgramWithSourceTy)(cl_context,
-                                                          cl_uint,
-                                                          const char**,
-                                                          const size_t*,
-                                                          cl_int*);
+        typedef cl_program (CL_CALLBACK *clCreateProgramWithSourceTy)(cl_context,
+                                                                      cl_uint,
+                                                                      const char**,
+                                                                      const size_t*,
+                                                                      cl_int*);
         clCreateProgramWithSourceTy clCreateProgramWithSourceU;
 
-        typedef cl_int (*clBuildProgramTy)(cl_program,
-                                           cl_uint,
-                                           const cl_device_id*,
-                                           const char*,
-                                           void (CL_CALLBACK *  /* pfn_notify */)(cl_program /* program */, void * /* user_data */),
-                                           void*);
+        typedef cl_int (CL_CALLBACK *clBuildProgramTy)(cl_program,
+                                                       cl_uint,
+                                                       const cl_device_id*,
+                                                       const char*,
+                                                       void (CL_CALLBACK *  /* pfn_notify */)(cl_program /* program */, void * /* user_data */),
+                                                       void*);
         clBuildProgramTy clBuildProgramU;
 
-        typedef cl_kernel (*clCreateKernelTy)(cl_program,
-                                              const char*,
-                                              cl_int*
-                                             );
-        clCreateKernelTy clCreateKernelU;
+        typedef cl_kernel (CL_CALLBACK *clCreateKernelTy)(cl_program,
+                                                          const char*,
+                                                          cl_int*
+                                                         );
+            clCreateKernelTy clCreateKernelU;
 
-        typedef cl_int (*clCreateKernelsInProgramTy)(cl_program,
-                                                     cl_uint,
-                                                     cl_kernel*,
-                                                     cl_uint*
-                                                    );
+        typedef cl_int (CL_CALLBACK *clCreateKernelsInProgramTy)(cl_program,
+                                                                 cl_uint,
+                                                                 cl_kernel*,
+                                                                 cl_uint*
+                                                                );
 
         clCreateKernelsInProgramTy clCreateKernelsInProgramU;
 
 
-        typedef cl_int (*clSetKernelArgTy)(cl_kernel,
-                                           cl_uint,
-                                           size_t,
-                                           const void*
-                                          );
+        typedef cl_int (CL_CALLBACK *clSetKernelArgTy)(cl_kernel,
+                                                       cl_uint,
+                                                       size_t,
+                                                       const void*
+                                                      );
         clSetKernelArgTy clSetKernelArgU;
 
-        typedef cl_int(*clEnqueueNDRangeKernelTy)(cl_command_queue,
-                                                  cl_kernel,
-                                                  cl_uint,
-                                                  const size_t*,
-                                                  const size_t*,
-                                                  const size_t*,
-                                                  cl_uint,
-                                                  const cl_event*,
-                                                  cl_event*
-                                                 );
+        typedef cl_int(CL_CALLBACK *clEnqueueNDRangeKernelTy)(cl_command_queue,
+                                                              cl_kernel,
+                                                              cl_uint,
+                                                              const size_t*,
+                                                              const size_t*,
+                                                              const size_t*,
+                                                              cl_uint,
+                                                              const cl_event*,
+                                                              cl_event*
+                                                             );
         clEnqueueNDRangeKernelTy clEnqueueNDRangeKernelU;
 
         UnderlyingCaller();
