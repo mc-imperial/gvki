12626bc93c9e13c53e67b60916c8ac091dadbdd4
Giulio Jiang <giulio.jiang14@imperial.ac.uk>
2015-07-06 11:48:27 +0100

    Logger now logs multiple flags and considers default case

--- lib/Logger.cpp
+++ lib/Logger.cpp
@@ -365,6 +365,21 @@ BufferInfo * Logger::tryGetBuffer(ArgInfo& ai) {
     return &buffers[mightBecl_mem];
 }
 
+static void recordMemFlags(std::ofstream* output, int flag, int clflag, int* firstFlagWritten, string flagstring)
+{
+    if (flag & clflag)
+    {
+        if (*firstFlagWritten)
+        {
+            *output << " | ";
+        } else
+        {
+            *firstFlagWritten = 1;
+        }
+        *output << flagstring;
+    }
+}
+
 void Logger::printJSONKernelArgumentInfo(ArgInfo& ai)
 {
     *output << "{";
@@ -401,25 +416,35 @@ void Logger::printJSONKernelArgumentInfo(ArgInfo& ai)
 
     if (BufferInfo * bi = tryGetBuffer(ai))
     {
+        
         *output << "\"type\": \"array\", ";
 
         *output << "\"size\": " << bi->size << ", ";
 
         *output << "\"flags\": \"";
-        switch (bi->flags)
+        
+//         LOGGING CL_MEMM_FLAGS
+
+        int firstFlagWritten = 0;
+        
+        if (bi->flags == 0)
         {
-            case CL_MEM_READ_ONLY:
-                *output << "CL_MEM_READ_ONLY";
-                break;
-            case CL_MEM_WRITE_ONLY:
-                *output << "CL_MEM_WRITE_ONLY";
-                break;
-            case CL_MEM_READ_WRITE:
-                *output << "CL_MEM_READ_WRITE";
-                break;
-            default:
-                *output << "UNKNOWN";
+            // default case -> CL_MEM_READ_WRITE
+            firstFlagWritten = 1;
+            *output << "CL_MEM_READ_WRITE";
+        } else
+        {
+            recordMemFlags(output, bi->flags, CL_MEM_READ_WRITE, &firstFlagWritten, "CL_MEM_READ_WRITE");
+            recordMemFlags(output, bi->flags, CL_MEM_WRITE_ONLY, &firstFlagWritten, "CL_MEM_WRITE_ONLY");
+            recordMemFlags(output, bi->flags, CL_MEM_READ_ONLY, &firstFlagWritten, "CL_MEM_READ_ONLY");
+            recordMemFlags(output, bi->flags, CL_MEM_USE_HOST_PTR, &firstFlagWritten, "CL_MEM_USE_HOST_PTR");
+            recordMemFlags(output, bi->flags, CL_MEM_ALLOC_HOST_PTR, &firstFlagWritten, "CL_MEM_ALLOC_HOST_PTR");
+            recordMemFlags(output, bi->flags, CL_MEM_COPY_HOST_PTR, &firstFlagWritten, "CL_MEM_COPY_HOST_PTR");
+            recordMemFlags(output, bi->flags, CL_MEM_HOST_WRITE_ONLY, &firstFlagWritten, "CL_MEM_HOST_WRITE_ONLY");
+            recordMemFlags(output, bi->flags, CL_MEM_HOST_READ_ONLY, &firstFlagWritten, "CL_MEM_HOST_READ_ONLY");
+            recordMemFlags(output, bi->flags, CL_MEM_HOST_NO_ACCESS, &firstFlagWritten, "CL_MEM_HOST_NO_ACCESS");
         }
+
         *output << "\"";
 
         if (bi->data != NULL)
